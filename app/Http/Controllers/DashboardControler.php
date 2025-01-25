3771a74f1eb15ee48e20827ee16ddc7ce7e0353e
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-25 23:29:15 +0000

    Edited Student function to Saerch for studentd

--- app/Http/Controllers/DashboardControler.php
+++ app/Http/Controllers/DashboardControler.php
@@ -2,24 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardControler extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $users = User::all();
         $teachers = User::where('role', 'teacher');
         $administrators = User::where('role', 'administration');
         return view('dashboard', compact('users', 'teachers', 'administrators'));
     }
-    public function teachers(){
+    public function teachers()
+    {
         return view('dash.teachers');
     }
-    public function administration(){
+    public function administration()
+    {
         return view('dash.administration');
     }
-    public function students(){
-        return view('dash.students');
+    public function students(Request $request)
+    {
+        $searchTerm = $request->search;
+        $students = Student::when($searchTerm, function($query, $searchTerm){
+            $query->where('name', 'like', "%$searchTerm%")
+            ->orwhere('class', 'like', "%$searchTerm%");
+        })->paginate(80);
+        return view('dash.students', compact('students'));
     }
 }
