255f8623ca861eb6dc842619547a9877e7835371
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-25 23:20:58 +0000

    Add Student Migration

--- database/migrations/2025_01_25_133933_create_students_table.php
+++ database/migrations/2025_01_25_133933_create_students_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name'); 
+            $table->string('phone'); 
+            $table->string('class');
+            $table->date('enrollment_date');
             $table->timestamps();
         });
     }
