6c36fd990a65000b1ba885e555c547c419432fd8
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-25 22:09:13 +0000

    Added status column to user table, factory and Model

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role')->default('principal');
+            $table->string('status')->default('pending');
             $table->rememberToken();
             $table->timestamps();
         });
