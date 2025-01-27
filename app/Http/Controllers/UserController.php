362fe318f5af698f2d643147fc4c6542da748289
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-27 00:04:21 +0000

    Updated the user>status and redirected user back

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function status(Request $request, User $user){
+    public function status(Request $request, $id){
+        $request->validate([
+            'status' => 'required|string|in:active,pending',
+        ]);
+        $user = User::findOrFail($id);
+
         $user->status = $request->status;
         $user->save();
          
