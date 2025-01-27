a71ee2077beab9eedf2bebddac1f60b4a4fc9a6e
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-27 18:11:42 +0000

    seeded data base

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Student;
 use App\Models\Subject;
+use App\Models\Teachers;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,7 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
-        Student::factory(10)->create();
+        Student::factory(5)->create();
+        Teachers::factory(5)->create();
         Subject::factory(8)->create();
         
     }
