45c7f8973c8bff6c79f7eff51d212e8c1c2e125c
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-27 00:10:34 +0000

    Search input for name and email Added

--- resources/views/dash/users.blade.php
+++ resources/views/dash/users.blade.php
@@ -9,7 +9,19 @@
             <h2 class="text-2xl font-semibold mb-6">Users</h2>
             <!-- Table -->
             <div class="overflow-x-auto">
-              <x-success />
+
+                <x-success />
+
+                <form method="GET" action="{{ route('users') }}" class="my-4 flex items-center space-x-4 w-full sm:w-1/2 lg:w-1/3 mx-auto">
+                    <input class="form-control rounded-lg border border-gray-300 shadow-sm p-2 w-full sm:w-auto" 
+                           type="text" name="search" placeholder="Search students..." 
+                           value="{{ request()->query('search') }}" 
+                           style="transition: all 0.3s ease-in-out;">
+                    <button class="btn btn-primary p-2 px-4 rounded-lg bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-400 transition duration-200" 
+                            type="submit">
+                        Search
+                    </button>
+                </form>
                 <table class="min-w-full bg-white border border-gray-300">
                     <thead class="bg-indigo-600 text-white">
                         <tr>
@@ -26,7 +38,8 @@
                                 <td class="py-3 px-6">{{ $user->name }}</td>
                                 <td class="py-3 px-6">{{ $user->email }}</td>
                                 <td class="py-3 px-6">
-                                    <form action="{{ route('users.status', $user->id) }}" method="POST" class="flex  align-baseline">
+                                    <form action="{{ route('users.status', $user->id) }}" method="POST"
+                                        class="flex  align-baseline">
                                         @csrf
                                         @method('PATCH')
                                         <select name="status" id="status"
@@ -43,13 +56,13 @@
                                             </svg>
                                         </button>
                                     </form>
-                                    
+
                                 </td>
                             </tr>
                         @endforeach
                         @error('status')
-                                      <div class="text-red-700 mt-2">{{ $message }}</div>   
-                                    @enderror
+                            <div class="text-red-700 mt-2">{{ $message }}</div>
+                        @enderror
                     </tbody>
                 </table>
             </div>
