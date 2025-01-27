bb26c193fc24fd8a75f1c96b3a9a85ea46fa76f4
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-27 16:31:58 +0000

    subject resource and classes route added to web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AdministrationController;
 use App\Http\Controllers\DashboardControler;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\TeachersController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\StudentMiddleware;
@@ -19,6 +20,9 @@ Route::patch('status/{id}', [UserController::class, 'status'])
 Route::get('dashboard/teacher', [DashboardControler::class, 'teachers'])
     ->middleware(['auth', 'verified'])
     ->name('teachers');
+Route::get('dashboard/classes', [DashboardControler::class, 'classes'])
+    ->middleware(['auth', 'verified'])
+    ->name('classes');
 Route::get('dashboard/administration', [DashboardControler::class, 'administration'])
     ->middleware(['auth', 'verified'])
     ->name('administration');
@@ -34,6 +38,8 @@ Route::resource('administration', AdministrationController::class)
     ->middleware(['auth', 'verified']);
 Route::resource('students', StudentController::class)
     ->middleware(StudentMiddleware::class);
+Route::resource('subjects', SubjectController::class)
+    ->middleware(['auth', 'verified']);
 
 
 Route::view('profile', 'profile')
