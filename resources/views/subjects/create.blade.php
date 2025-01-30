f090e15ab8a2a6c57ef9d19f3f187d0d04c586e6
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-30 19:16:27 +0000

    Create Subject page
    Add x-app-layout
    add x-side
    add form with route subject.store
    add one input name with error message

--- resources/views/subjects/create.blade.php
+++ resources/views/subjects/create.blade.php
@@ -1,3 +1,33 @@
-<div>
-    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
-</div>
+<x-app-layout>
+    <div class="flex h-screen">
+        <x-side></x-side>
+        <div class="flex-1 p-6">
+
+            <h2 class="text-2xl font-semibold mb-6">Add Student</h2>
+            <!-- Table -->
+            <div class="overflow-x-auto">
+                <form action="{{ route('subjects.store') }}" method="POST"
+                    class="card w-1/2 mx-auto p-8 bg-white shadow-lg rounded-lg space-y-4">
+                    @csrf
+
+                    <div class="flex flex-col space-y-2">
+                        <label for="name" class="font-semibold text-gray-700">Name</label>
+                        <input type="text" name="name"
+                            class="form-control border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            value="{{ old('name') }}">
+                        @error('name')
+                            <div class="text-red-500 text-sm">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <button type="submit"
+                        class="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 mt-4">
+                        Create
+                    </button>
+                </form>
+            </div>
+
+
+
+        </div>
+    </div>
+</x-app-layout>
