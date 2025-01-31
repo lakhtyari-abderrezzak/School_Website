1a5592728482f7f9d08fa01edcf8eb624c9930cd
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-01-31 19:30:44 +0000

    Add isAdmin to middleware goupe in some route

--- routes/web.php
+++ routes/web.php
@@ -12,34 +12,34 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 
 Route::get('dashboard', [DashboardControler::class, 'index'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'verified', 'isAdmin'])
     ->name('dashboard');
 Route::patch('status/{id}', [UserController::class, 'status'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'verified', 'isAdmin'])
     ->name('users.status');
 Route::get('dashboard/teacher', [DashboardControler::class, 'teachers'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'verified', 'isAdmin'])
     ->name('teachers');
 Route::get('dashboard/classes', [DashboardControler::class, 'classes'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'verified', 'isAdmin'])
     ->name('classes');
 Route::get('dashboard/administration', [DashboardControler::class, 'administration'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'verified', 'isAdmin'])
     ->name('administration');
 Route::get('dashboard/student', [DashboardControler::class, 'students'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'verified', 'isAdmin'])
     ->name('students');
 Route::get('dashboard/users', [DashboardControler::class, 'users'])
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth', 'verified', 'isAdmin'])
     ->name('users');
 Route::resource('teachers', TeacherController::class)
-    ->middleware(['auth', 'verified']);
+    ->middleware(['auth', 'verified', 'isAdmin']);
 Route::resource('administration', AdministrationController::class)
-    ->middleware(['auth', 'verified']);
+    ->middleware(['auth', 'verified', 'isAdmin']);
 Route::resource('students', StudentController::class)
     ->middleware(StudentMiddleware::class);
 Route::resource('subjects', SubjectController::class)
-    ->middleware(['auth', 'verified']);
+    ->middleware(['auth', 'verified', 'isAdmin']);
 
 
 Route::view('profile', 'profile')
