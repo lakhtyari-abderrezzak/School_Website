ba32b4d4176b95ecdb35e52881593f68d5374fb5
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-02-02 23:18:02 +0000

    Style: changed the layout of sid-bar

--- resources/views/components/side.blade.php
+++ resources/views/components/side.blade.php
@@ -1,25 +1,18 @@
-<!-- Sidebar -->
-<div class="w-64 bg-indigo-600 text-white p-4">
-    <h1 class="text-3xl font-semibold mb-8">Dashboard</h1>
-      <div class="inline-block w-full">
-        <x-nav-link class="text-white text-xl w-full mt-4" :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-            {{ __('Dashboard') }}
-        </x-nav-link>
-        <x-nav-link class="text-white text-xl w-full mt-4" :href="route('users')"   :active="request()->routeIs('users')" wire:navigate>
-            {{ __('Users') }}
-        </x-nav-link>
-        <x-nav-link class="text-white text-xl w-full mt-4" :href="route('classes')"   :active="request()->routeIs('classes')" wire:navigate>
-            {{ __('Class') }}
-        </x-nav-link>
-        <x-nav-link class="text-white text-xl w-full mt-4" :href="route('students')"   :active="request()->routeIs('students')" wire:navigate>
-            {{ __('Students') }}
-        </x-nav-link>
-        <x-nav-link class="text-white text-xl w-full mt-4"  :href="route('teachers')" :active="request()->routeIs('teachers')" wire:navigate>
-            {{ __('Teachers') }}
-        </x-nav-link>
-        <x-nav-link class="text-white text-xl w-full mt-4" :href="route('administration')" :active="request()->routeIs('administration')" wire:navigate>
-            {{ __('Administration') }}
-        </x-nav-link>
+<div id="sidebar" class="fixed inset-0 z-50 lg:w-64 w-0 lg:flex flex-col bg-gray-800 text-white transition-all duration-300 ease-in-out">
+    <div class="flex justify-between items-center p-4 border-b border-gray-700">
+        <h2 class="text-2xl font-semibold">Dashboard</h2>
+        <button id="close-btn" class="lg:hidden text-white">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+          </svg>
+        </button>
       </div>
-
-</div>
+      <ul class="space-y-2 p-4">
+        <x-nav-link :active="request()->is('dashboard')" :href="route('dashboard')" class="block py-2 px-4  "  wire:navigate >Dashboard</x-nav-link>
+        <x-nav-link :active="request()->is('dashboard/users')" :href="route('users')" class="block py-2 px-4  " wire:navigate >Users</x-nav-link>
+        <x-nav-link :active="request()->is('dashboard/students')" :href="route('students')" class="block py-2 px-4  " wire:navigate >Students</x-nav-link>
+        <x-nav-link :active="request()->is('dashboard/teachers')" :href="route('teachers')" class="block py-2 px-4  " wire:navigate >Teachers</x-nav-link>
+        <x-nav-link :active="request()->is('dashboard/administration')" :href="route('administration')" class="block py-2 px-4  " wire:navigate >Administration</x-nav-link>
+      </ul>
+  
+  </div>
