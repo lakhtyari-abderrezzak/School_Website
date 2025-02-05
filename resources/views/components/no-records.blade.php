1f3c56fad3ccb27adb9cbd55c8adf94b5828d284
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-02-05 11:33:12 +0000

    Create x-no-records component to display 'No Records Found' message

---
+++ resources/views/components/no-records.blade.php
@@ -0,0 +1,3 @@
+<div class="flex items-center justify-center p-6 bg-gray-100 border border-gray-300 rounded-lg shadow-md my-4">
+    <p class="text-xl text-gray-600 font-semibold">No Records Found</p>
+</div>
