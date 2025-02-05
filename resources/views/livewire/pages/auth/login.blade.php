6d9861f33f9ead0c0338b0680324164e2d548bd6
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-02-05 11:48:50 +0000

    Formmat: code in login.blade.php

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -34,7 +34,7 @@ new #[Layout('layouts.guest')] class extends Component {
     }
 }; ?>
 
-<div>
+<div >
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
