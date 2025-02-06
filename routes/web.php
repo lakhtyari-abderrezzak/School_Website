360082addd36878a6a74e8d06b818a5ece10014c
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-02-06 23:39:47 +0000

    Add : views for services, about and contact route with submitForm

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdministrationController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\DashboardControler;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\SubjectController;
@@ -9,7 +10,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Middleware\StudentMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'home')->name('home');
 
 Route::get('dashboard', [DashboardControler::class, 'index'])
     ->middleware(['auth', 'verified', 'isAdmin'])
@@ -49,6 +50,11 @@ Route::resource('students', StudentController::class)
     ->middleware(['auth', 'verified', 'isAdmin']);
 Route::resource('subjects', SubjectController::class)
     ->middleware(['auth', 'verified', 'isAdmin']);
+Route::get('contact', [ContactController::class, 'contact'])->name('contact');
+Route::post('contact', [ContactController::class, 'submitForm'])->name('submitForm');
+
+Route::view('about', 'about')->name('about');
+Route::view('services', 'services')->name('services');
 
 
 
@@ -56,4 +62,4 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
