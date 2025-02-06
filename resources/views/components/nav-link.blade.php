6af031d3a9911485b2be6e13b11f5f6624371aad
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-02-06 23:20:18 +0000

    Add: wire:navigate to nav-lik

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,10 +3,10 @@
 @php
     $classes = ($active ?? false)
                 ? 'bg-gray-700 text-white hover:bg-gray-600 transition duration-150 ease-in-out px-4 py-2 rounded-md'
-                : ' text-gray-200 hover:bg-gray-700 transition duration-150 ease-in-out px-4 py-2 rounded-md';
+                : ' text-black-200 hover:bg-gray-700 transition duration-150 ease-in-out px-4 py-2 rounded-md';
 @endphp
 
-<a {{ $attributes->merge(['class' => $classes]) }}>
+<a wire:navigate {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
 </a>
 
