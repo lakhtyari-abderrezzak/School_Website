c159874b20552ec9996c21c80418e63df49197c8
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-02-07 00:05:35 +0000

    Added email view template for contact form submission

---
+++ resources/views/emails/email.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Contact Form Submission</title>
+</head>
+<body>
+
+<h1>{{ $data['subject'] }}</h1>
+
+<p><strong>Name:</strong> {{ $data['name'] }}</p>
+<p><strong>Email:</strong> {{ $data['email'] }}</p>
+<p><strong>Message:</strong> {{ $data['message'] }}</p>
+
+</body>
+</html>
+
